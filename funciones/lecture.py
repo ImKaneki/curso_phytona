cc8b405972d2263932c21f97476f1b3b634863fb
ImKaneki <josemgarriazo@gmail.com>
2024-06-10 11:02:10 -0500

    trabajo

---
+++ funciones/lecture.py
@@ -0,0 +1,11 @@
+# return devuelve valores que podre hacer uso
+# crear una funcion que retorne el numero 10, y muestre por terminal si es par
+# siempre que el valor retorne mi funcion se utilice en mas sentencias u operaciones hacer uso de return
+def diez():
+    return 10
+if diez()%2==0
+    print("es par")
+else:
+    print("es impar")
+#print solo muestra por terminal 
+**ejemplo**
