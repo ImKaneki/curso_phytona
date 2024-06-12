3ef569f9e054c99f9be60cf792fbd7d60fd411ba
ImKaneki <josemgarriazo@gmail.com>
2024-06-12 10:28:44 -0500

    trabajo

--- funciones/lecture.py
+++ funciones/lecture.py
@@ -9,3 +9,42 @@ else:
     print("es impar")
 #print solo muestra por terminal 
 **ejemplo**
+# return cuando queremos que nuestra funcion devuelva o retorne un tipo de dato o un tipo de dato estructurado
+
+# crear una funcion que me muestre el producto de dos numeros
+def producto(a,b):
+    return a*b
+print(producto(4,8))
+
+# crear una funcion que me retorne una lista de tres numeros 
+def lista_numeros():
+    return [3,2,6]
+# crear una funcion que  por parametro reciba un nombre y reciba un nombre y retorne un mensaje de bienvenida con el nombre
+def mensaje (nombre)
+    print(f"hola, {nombre},bienvenido al colegio")
+mensaje("jose")
+
+# crear una funcion que reciba por parametro una lista de numeros y me devuelva el numero menor, mostrar por terminal el valor retornado por la funcion
+lista=[4,3,6,78,7]
+def Min(1)
+    minimo=1[0]
+    for n in 1:
+        if n < minimo:
+            minimo=n
+        return minimo
+print(Min(lista))
+
+# crear una funcion que reciba como parametro el nombre y la edad de una persona mi funcion debera tener 
+nombre=input("ingrese su nombre: ")
+edad=int(input("ingrese su edad")
+def persona(nom,edad):
+    # return {
+    #   "nombre":nom,
+#       "edad":edad
+#  }
+return dict(
+    nombre=nom,
+    edad=edad   
+)
+print(persona(nombre,edad)
+       
