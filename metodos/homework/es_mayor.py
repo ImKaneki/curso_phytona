6d48d0134513c2bd7fce987569b6af67fe93564e
ImKaneki <josemgarriazo@gmail.com>
2024-07-15 10:34:31 -0500

    trabajo

---
+++ metodos/homework/es_mayor.py
@@ -0,0 +1,9 @@
+def es_mayor(edad):
+    """# funcion para saber si una persona es mayor de edad"""
+    if edad >=18:
+        print("es mayor de edad")
+    else:
+        print("es menor de edad")
+
+
+    def es_mayor(lista):
---
+++ modulo/lecture.py
@@ -0,0 +1,15 @@
+# practicando pylint
+nombre_alumno:str="miguel"
+
+def suma(a:int,b:int)->int:
+    """funcion para sumar dos numeros"""
+    return a+b
+print(suma(4,7))
+
+def resta(a,b):
+    """funcion para restar dos numeros"""
+    return a-b
+def producto(a,b):
+    """funcion de producto"""
+    return a*b
+mensaje:str="hola"
